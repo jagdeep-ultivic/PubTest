585c5b3c91f85502834d42987aeeaa7990fb84db
harmeetsohi27 <harmeetsohi27@gmail.com>
2023-12-14 10:26:30 +0530

    update library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.ultivic.sample.pubtest"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.ultivic.sample.pubtest"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
