b187abafb31f053bd9b93e87a00d7e1e0d10c4e5
harmeetsohi27 <harmeetsohi27@gmail.com>
2023-12-14 10:39:53 +0530

    update lib

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -22,11 +22,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -43,7 +43,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.jagdeep-ultivic"
             artifactId = " com.and.ploader.mylibrary"
-            version = "1.0.2"
+            version = "1.0.22"
         }
     }
 }
